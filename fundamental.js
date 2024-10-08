424c51d16dc3dc138f797915d6bbdc7da8fd45bf
MTofayelAhmed <tofayelahmed324@gmail.com>
2024-10-08 13:29:05 +0200

    first commit

---
+++ fundamental.js
@@ -0,0 +1,23 @@
+1. // how to declare variable 
+// let and const use case 
+
+// 2. conditions <, > , === , !== , <=, >=
+// multiple condition && or  ||
+
+// condition example 
+
+const fatherName = "Arnold";
+let season = "winter"
+if (fatherName=== "arnold" || season === "winter"){
+
+}
+
+else if (fatherName === "Arnold"){
+
+}
+
+else {
+
+}
+
+
